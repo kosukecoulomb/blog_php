42e184be9c635cf7b078db3fcad8cb6610d2de7d
kosukecoulomb <jobch.coulomb@gmail.com>
2022-08-05 20:43:01 +0900

    [add]データベース管理クラス追加

---
+++ lib/connect.php
@@ -0,0 +1,29 @@
+<?php
+
+class connect
+{
+    const DB_NAME = "blog";
+    const HOST = "localhost";
+    const USER = "user";
+    const PASS = "pass";
+
+    private $dbh;
+
+    public function __construct()
+    {
+        $dsn = "mysql:host=" . self::HOST . ";dbname=" . self::DB_NAME . ";charset=utf8mb4";
+        try {
+            $this->dbh = new PDO($dsn, self::USER, self::PASS);
+        } catch (Exception $e) {
+            exit($e->getMessage());
+        }
+        $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    }
+
+    public function query($sql, $param = null)
+    {
+        $stmt = $this->dbh->prepare($sql);
+        $stmt->execute($param);
+        return $stmt;
+    }
+}
