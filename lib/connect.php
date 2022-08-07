a27385a3951cff32a2e96e45fac53fcc5a2e200d
kosukecoulomb <jobch.coulomb@gmail.com>
2022-08-07 15:13:11 +0900

    [add]投稿機能

--- lib/connect.php
+++ lib/connect.php
@@ -1,7 +1,5 @@
 <?php
-
-class connect
-{
+class connect{
     const DB_NAME = "blog";
     const HOST = "localhost";
     const USER = "user";
@@ -9,19 +7,19 @@ class connect
 
     private $dbh;
 
-    public function __construct()
-    {
-        $dsn = "mysql:host=" . self::HOST . ";dbname=" . self::DB_NAME . ";charset=utf8mb4";
-        try {
-            $this->dbh = new PDO($dsn, self::USER, self::PASS);
-        } catch (Exception $e) {
-            exit($e->getMessage());
-        }
-        $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    public function __construct(){
+    $dsn = "mysql:host=".self::HOST.";dbname=".self::DB_NAME.";charset=utf8mb4";
+    try {
+        $this->dbh = new PDO($dsn, self::USER, self::PASS);
+
+    } catch(Exception $e){
+        exit($e->getMessage());
     }
 
-    public function query($sql, $param = null)
-    {
+    $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    }
+
+    public function query($sql, $param = null){
         $stmt = $this->dbh->prepare($sql);
         $stmt->execute($param);
         return $stmt;
---
+++ post.php
@@ -0,0 +1,107 @@
+<?php
+include 'lib/secure.php';
+include 'lib/connect.php';
+
+$title = "";        // タイトル
+$body = "";         // 本文
+$title_alert = "";  // タイトルのエラー文言
+$body_alert = "";   // 本文のエラー文言
+
+if (!empty($_POST['title']) && !empty($_POST['body'])) {
+    // titleとbodyがPOSTメソッドで送信されたとき
+    $title = $_POST['title'];
+    $body = $_POST['body'];
+    $db = new connect();
+    $sql = "INSERT INTO articles (title, body, created_at, updated_at)
+            VALUES (:title, :body, NOW(), NOW())";
+    $result = $db->query($sql, array(':title' => $title, ':body' => $body));
+    header('Location: backend.php');
+} else if (!empty($_POST)) {
+    if (!empty($_POST['title'])) {
+        $title = $_POST['title'];
+    } else {
+        $title_alert = "タイトルを入力してください。";
+    }
+
+    if (!empty($_POST['body'])) {
+        $body = $_POST['body'];
+    } else {
+        $body_alert = "本文を入力してください。";
+    }
+}
+?>
+
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Blog Backend</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        body {
+            padding-top: 5rem;
+        }
+
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+
+        .bg-red {
+            background-color: purple !important;
+        }
+    </style>
+
+    <!-- Custom styles for this template -->
+    <link href="./css/blog.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <?php include('lib/nav.php'); ?>
+
+    <main class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+                <h1>記事の投稿</h1>
+
+                <form action="post.php" method="post">
+                    <div class="mb-3">
+                        <label class="form-label">タイトル</label>
+                        <?php echo !empty($title_alert) ? '<div class="alert alert-danger">' . $title_alert . '</div>' : '' ?>
+                        <input type="text" name="title" value="<?php echo $title; ?>" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">本文</label>
+                        <?php echo !empty($body_alert) ? '<div class="alert alert-danger">' . $body_alert . '</div>' : '' ?>
+                        <textarea name="body" class="form-control" rows="10"><?php echo $body; ?></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <button type="submit" class="btn btn-primary">投稿する</button>
+                    </div>
+                </form>
+
+            </div>
+
+        </div><!-- /.row -->
+
+    </main><!-- /.container -->
+
+</body>
+
+</html>
